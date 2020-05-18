05c448c000ad361af8635c6d566c6d5e837306dc
KTyH <archlis21@gmail.com>
2020-05-18 14:02:16 +0300

    calorie calc react add

---
+++ react/calorie-calc/precache-manifest.4de97806271463a2d7b49766f34dc137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298738a5c01f851fb6a804c3057e1841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67776946df42348999db",
+    "url": "/static/css/main.c86b3477.chunk.css"
+  },
+  {
+    "revision": "c0b0e1d7da7011a66847",
+    "url": "/static/js/2.e0311931.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e0311931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67776946df42348999db",
+    "url": "/static/js/main.709fe038.chunk.js"
+  },
+  {
+    "revision": "c1758551f55e3fbf74a6",
+    "url": "/static/js/runtime-main.a23fd89e.js"
+  }
+]);

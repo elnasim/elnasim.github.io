580e80ac0a20bb27e959aad50e72463bbb5e073f
Serega KTyH <archlis21@gmail.com>
2020-08-20 16:57:53 +0300

    invoice

---
+++ react/invoice.test/precache-manifest.f2682f1fbbd771b10b3583654b516045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfeacabba7b6f435b75c79233fa8b0db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46212c1d22b56b9205bd",
+    "url": "/static/css/main.29780a1e.chunk.css"
+  },
+  {
+    "revision": "1c8b4dbfb67a17486818",
+    "url": "/static/js/2.4a458935.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4a458935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46212c1d22b56b9205bd",
+    "url": "/static/js/main.c2d3517d.chunk.js"
+  },
+  {
+    "revision": "76c7c6efa1f29bf20fcc",
+    "url": "/static/js/runtime-main.47f79ace.js"
+  }
+]);
